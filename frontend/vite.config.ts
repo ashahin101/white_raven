b241722a2fe98d623b502edf3a2531dcdab8f79b
ashahin101 <a.shahin@dinffo.com>
2026-03-09 22:51:49 +0300

    made sure backend and frontend: dev, builds, start all works fine

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   server: {
-    // port: 3000,
+    port: 3000,
     // cors: {
     //   // the origin you will be accessing via browser
     //   origin: 'http://localhost:8081/',
